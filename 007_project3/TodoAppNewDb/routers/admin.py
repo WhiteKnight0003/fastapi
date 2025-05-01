d6b974dfa1807d7934b50264fbbb4c60c0778e4f
WhiteKnight0003 <whileknight0003@gmail.com>
2025-05-01 21:53:05 +0700

    authenticate

---
+++ 007_project3/TodoAppNewDb/routers/admin.py
@@ -0,0 +1,49 @@
+from fastapi import APIRouter , Depends, HTTPException, Path
+from starlette import status
+from pydantic import BaseModel, Field
+from models import Todos
+from database  import SessionLocal
+from typing import Annotated
+from sqlalchemy.orm import Session
+
+# lấy thằng user mới được xác thực
+from .auth import get_current_user
+
+router = APIRouter(
+    prefix='/admin',
+    tags=['admin'],
+)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
+
+@router.get('/todos', status_code=status.HTTP_200_OK)
+async def read_all(user: user_dependency, db: db_dependency):
+    # cái get_current_user trả ra các trường gì thì get theo cái đos
+    if user is None or user.get('role') != 'admin':
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    return db.query(Todos).all()
+
+# @router.get('/todo/{todo_id}', status_code=status.HTTP_200_OK)
+# async def read_user(user: user_dependency, db: db_dependency):
+#     if user is None or user.get('user_role') != 'admin':
+#         raise
+
+@router.delete('/todo/{todo_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_user(user: user_dependency, db: db_dependency, todo_id: int = Path(gt=0)):
+    if user is None or user.get('role') != 'admin':
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=404, detail='Todo not found')
+    
+    db.delete(todo_model)
+    db.commit()
+    
--- 007_project3/TodoAppNewDb/routers/auth.py
+++ 007_project3/TodoAppNewDb/routers/auth.py
@@ -66,11 +66,12 @@ def authenticate_user(username: str, password: str, db):
 Tạo JWT chứa payload gồm:
 sub: username (chủ thể)
 id: user id
+role : vai trò của ng đang muốn xác thưcj
 exp: thời gian hết hạn
 Mã hóa bằng SECRET_KEY với HS256.
 """
-def create_access_token(username: str, user_id: int, expires_delta: timedelta):
-        encode = {'sub': username, 'id': user_id}
+def create_access_token(username: str, user_id: int, role : str,expires_delta: timedelta):
+        encode = {'sub': username, 'id': user_id, 'role': role} 
         expires = datetime.utcnow() + expires_delta
         encode.update({'exp': expires})
         return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
@@ -96,12 +97,14 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get('sub')
         user_id: int = payload.get('id')
+        user_role : str = payload.get('role')
+
         if username is None or user_id is None:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail='Could not validate user'
             )
-        return {'username': username, 'id': user_id}
+        return {'username': username, 'id': user_id, 'role':user_role}
     except JWTError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
                             detail='Could not validate user')
@@ -147,7 +150,7 @@ async def login_for_access_token(
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
                             detail='Could not validate user')
-    token = create_access_token(user.username, user.id, timedelta(minutes=20))
+    token = create_access_token(user.username, user.id, user.role, timedelta(minutes=20))
     return {'access_token': token, 'token_type': 'bearer'}
     
 
