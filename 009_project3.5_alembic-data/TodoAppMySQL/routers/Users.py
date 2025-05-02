c720fa91aa67ed9dea5dda41105a3178133b518c
WhiteKnight0003 <whileknight0003@gmail.com>
2025-05-03 00:09:38 +0700

    alembic complete

--- 009_project3.5_alembic-data/TodoAppMySQL/routers/Users.py
+++ 009_project3.5_alembic-data/TodoAppMySQL/routers/Users.py
@@ -33,6 +33,7 @@ class UserVerification(BaseModel):
 
 
 
+
 @router.get('/', status_code=status.HTTP_200_OK)
 async def get_user(user: user_dependency, db: db_dependency):
     if user is None:
@@ -53,5 +54,16 @@ async def change_password(user: user_dependency, db: db_dependency,
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Error on password change')
     
     user_model.hashed_password = bcrypt_context.hash(user_verification.new_password)
+    db.add(user_model)
+    db.commit()
+
+@router.put('/phone/{phone_number}', status_code=status.HTTP_204_NO_CONTENT)
+async def change_phone(user:user_dependency, db: db_dependency, phone_number: str ):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
+                            detail='Could not validate user')
+    
+    user_model = db.query(User).filter(User.id == user.get('id')).first()
+    user_model.phone_number = phone_number
     db.add(user_model)
     db.commit()
